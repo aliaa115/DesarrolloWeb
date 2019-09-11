ff61157fa50453f1c8b957bdc4bc4dbf9d571ba8
U. Mariano Gálvez <umg@labs.academico>
2019-09-11 09:58:53 -0600

    33

--- Clase0906/PaginaWeb/JS/script.js
+++ Clase0906/PaginaWeb/JS/script.js
@@ -17,11 +17,66 @@ var numPor = document.getElementById("*");
 
 var resul = document.getElementById("pResultado");
 
+var valor1 = 0;
+var valor2 = 0;
+var accion = "";
+var estado = 1;
+var operacionAccionada = 0;
 
 var botNum = function(num){
-    resul.innerText = resul.innerText + (num)
+    if(estado == 1){
+        resul.innerText = resul.innerText + (num);
+    }
+    if(estado == 0){
+        estado =1;
+        resul.innerText = num;
+    }
 }
 
 var borrar = function(){
     resul.innerText = "";
+    valor1 = 0;
+    valor2 = 0;
+    accion = "";
 }
+
+var restar = function(){
+    if (accion == ""){
+        valor1 = parseFloat(resul.innerText);
+        console.log(valor1, valor2, accion);
+        estado = 0;
+    }
+    if(accion == "-"){
+        estado = 0;
+        valor2 = valor1 - parseFloat(resul.innerText);
+        valor1 = valor2;
+        console.log(valor1, valor2, accion);
+        resul.innerText = valor2; 
+    }
+    if(accion == "+"){
+        estado = 0;
+        valor2 = valor1 + parseFloat(resul.innerText);
+        console.log(valor1, valor2, accion);
+        resul.innerText = valor2; 
+    }
+    accion = "-";
+}
+
+var suma = function(){
+    valor1 = parseInt(resul.innerText);
+    accion = "+";
+    resul.innerText = "";
+}
+
+var mostrarresultado = function(){
+    if(operacionAccionada ==0){
+        if (accion == "-"){
+            valor2 = valor1 - parseFloat(resul.innerText);
+        }
+    }
+    console.log(valor1, valor2, accion);
+    estado = 0;
+    accion = "";
+    resul.innerText = valor2; 
+}
+
